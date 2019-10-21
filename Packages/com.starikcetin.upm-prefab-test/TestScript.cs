8c7392addc3c2d3d00890cd0f9fc5c0be1ab489e
starikcetin <cetinsamedtarik@gmail.com>
2019-10-22 01:52:58 +0300

    test ready

---
+++ Packages/com.starikcetin.upm-prefab-test/TestScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.Log("It seems to be working, boss!");
+    }
+}
